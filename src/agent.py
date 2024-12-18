6f7ffdff167eabb0e4d027a8d2f31f799a8929fb
Ania Blanco <abr04work@gmail.com>
2024-12-18 22:23:20 +0100

    feat(src): añadido código base a los archivos main, game_engine y agent.

--- src/agent.py
+++ src/agent.py
@@ -1,1 +1,8 @@
 # Contains the logic for the computer agent's decision-making strategy.
+
+def get_computer_action():
+    computer_selection = random.randint(0, len(GameAction) - 1)
+    computer_action = GameAction(computer_selection)
+    print(f"Computer picked {computer_action.name}.")
+
+    return computer_action
--- src/main.py
+++ src/main.py
@@ -1,1 +1,21 @@
 # Entry point of the program; coordinates the game flow.
+
+def main():
+
+    while True:
+        try:
+            user_action = get_user_action()
+        except ValueError:
+            range_str = f"[0, {len(GameAction) - 1}]"
+            print(f"Invalid selection. Pick a choice in range {range_str}!")
+            continue
+
+        computer_action = get_computer_action()
+        assess_game(user_action, computer_action)
+
+        if not play_another_round():
+            break
+
+
+if __name__ == "__main__":
+    main()
