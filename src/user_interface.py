5422c05fd77437604d37743be7bd697bafc5d787
Ania Blanco <abr04work@gmail.com>
2025-01-06 19:11:27 +0100

    fix(user_interface): se validan las entradas del usuario para evitar ValueErrors.

--- src/user_interface.py
+++ src/user_interface.py
@@ -5,10 +5,14 @@ from game_engine import GameAction
 def get_user_action():
     game_choices = [f"{game_action.name}[{game_action.value}]" for game_action in GameAction]
     game_choices_str = ", ".join(game_choices)
-    user_selection = int(input(f"\nPick a choice ({game_choices_str}): "))
-    user_action = GameAction(user_selection)
-
-    return user_action
+    
+    while True:
+        user_input = input(f"\nPick a choice ({game_choices_str}): ")
+        try:
+            user_selection = int(user_input)
+            if user_selection in range(len(GameAction)): return GameAction(user_selection)
+            else: print(f"Invalid selection. Pick a choice in range [0, {len(GameAction) - 1}]!")
+        except ValueError: print(f"Invalid input. Please enter a number between 0 and {len(GameAction) - 1}.")
 
 def play_another_round():
     another_round = input("\nAnother round? (y/n): ")
